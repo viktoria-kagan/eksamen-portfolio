88d174a78c2347685c6440e0a5d5a19a910c68f1
vika <vika0001@stud.kea.dk>
2025-01-08 03:13:20 +0100

    update

--- js/script.js
+++ js/script.js
@@ -9,18 +9,18 @@ burger.addEventListener("click", () => {
   T1.classList.toggle("close");
 });
 
-document.querySelector("#boks").addEventListener("click", pauseAnimation);
+// document.querySelector("#boks").addEventListener("click", pauseAnimation);
 
-function pauseAnimation() {
-  document.querySelector("#boks").classList.add("paused");
-  document.querySelector("#boks").removeEventListener("click", pauseAnimation); // Fjern denne eventlistener
-  document.querySelector("#boks").addEventListener("click", resumeAnimation); // Tilføj eventlistener for at starte igen
-}
-function resumeAnimation() {
-  document.querySelector("#boks").classList.remove("paused");
-  document.querySelector("#boks").removeEventListener("click", resumeAnimation); // Fjern denne eventlistener
-  document.querySelector("#boks").addEventListener("click", pauseAnimation); // Tilføj eventlistener for at pause igen
-}
+// function pauseAnimation() {
+//   document.querySelector("#boks").classList.add("paused");
+//   document.querySelector("#boks").removeEventListener("click", pauseAnimation); // Fjern denne eventlistener
+//   document.querySelector("#boks").addEventListener("click", resumeAnimation); // Tilføj eventlistener for at starte igen
+// }
+// function resumeAnimation() {
+//   document.querySelector("#boks").classList.remove("paused");
+//   document.querySelector("#boks").removeEventListener("click", resumeAnimation); // Fjern denne eventlistener
+//   document.querySelector("#boks").addEventListener("click", pauseAnimation); // Tilføj eventlistener for at pause igen
+// }
 
 // function addPaused() {
 //   box.classList.add(".paused"); // Stop rotation
